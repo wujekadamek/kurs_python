5431fe9b3551cbc3be729b460a52a4fbc7788472
wujekadamek <98348467+wujekadamek@users.noreply.github.com>
2026-06-11 12:26:22 +0200

    Add files via upload

---
+++ 03_if_and_loops/if_elif_else.py
@@ -0,0 +1,37 @@
+num = -20
+
+if num >= 3:
+    print("num wieksze lub rowne 3.")
+    print("oraz !")
+
+if num == 5:
+    print("5 = 5")
+
+if num == 1:
+    print("num = 1")
+elif num == 2:
+    print("num = 2")
+elif num == 3:
+    print("num = 3")
+elif num == 4:
+    print("num = 4")
+elif num == 5:
+    print("num = 5")
+elif num == 10:
+    print("num = 10")
+elif num >= 11:
+    print("num >= 11")
+else:
+    print("domyslny kod, jesli reszta porownan da False.")
+
+if 5 > 1: print("5 > 1")
+
+print("dalszy kod")
+
+if 10 > 2:
+    print("10 > 2")
+    if 4 > 1:
+        print("4 > 1")
+        if 3 < 2:
+            print("3 < 2")
+        print("4 > 1 c.d.")
---
+++ 03_if_and_loops/if_short_check.py
@@ -0,0 +1,21 @@
+flag = True
+
+if flag == True:
+    print("flag rowne jest true")
+
+if flag is True:
+    print("flag to true")
+
+if flag:
+    print("flag to jest true")
+
+flag = False
+
+if flag != True:
+    print("flag jest false")
+
+if flag is not True:
+    print("flag jest false")
+
+if not flag:
+    print("flag jest fałszem")

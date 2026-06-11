0c32e8232ddb8ffa3b65ab708a8f47a63daab3ce
wujekadamek <98348467+wujekadamek@users.noreply.github.com>
2026-06-11 12:24:17 +0200

    Add files via upload

---
+++ 02_operators/comparison_operators.py
@@ -0,0 +1,27 @@
+# operatory porownania
+
+result = 12 == 4
+print(result)
+
+print(12 != 4) # True
+print(12 != 12) # False
+
+print(8 > 1) # True
+print(7 > 10) # False
+print(2 < 10) # True
+print(5 >= 3) # True
+print(5 >= 5) # True
+print(10 <= 10) # True
+print(9 <= 10) # True
+print(20 <= 10) # False
+
+if 10 > 5:
+    print("10 jest wieksze od 5.")
+
+a = 12
+b = 80
+
+if b < a:
+    print("b jest mniejsze od a")
+else:
+    print("b nie jest mniejsze od a")
---
+++ 02_operators/concatenation_operator.py
@@ -0,0 +1,8 @@
+# operator konkatenacji
+
+strData = "Hello " + "World!" + "!"
+print(strData)
+print(strData + " and Hello again!")
+
+listData = [1,2,3,4]
+print(listData + [21,37,67,69,420])
